e36962e6574d6834f15f692918c3262fb6b61fd9
kim dawoon <dwkim1@greenlabs.co.kr>
2022-02-13 16:37:18 +0900

    list data 연결

--- src/components/list.vue
+++ src/components/list.vue
@@ -1,8 +1,8 @@
 <template>
   <v-list density="compact">
-    <v-list-subheader>Sale items</v-list-subheader>
+    <v-card-title class="text-xs-h6 text-md-h5 text-lg-h4">판매 리스트</v-card-title>
     <v-list-item v-for="(item, i) in items" :key="i" :value="item" active-color="primary">
-      <listItem />
+      <listItem :title="item.title" :category="item.category" :desc="item.text" />
     </v-list-item>
   </v-list>
 </template>
@@ -16,13 +16,48 @@ export default defineComponent({
   data() {
     return {
       items: [
-        { titel: "Lorem ipsum", text: "Real-Time", icon: "mdi-clock" },
-        { titel: "Lorem ipsum", text: "Audience", icon: "mdi-account" },
-        { titel: "Lorem ipsum", text: "Conversions", icon: "mdi-flag" },
-        { titel: "Lorem ipsum", text: "Conversions", icon: "mdi-flag" },
-        { titel: "Lorem ipsum", text: "Conversions", icon: "mdi-flag" },
-        { titel: "Lorem ipsum", text: "Conversions", icon: "mdi-flag" },
-        { titel: "Lorem ipsum", text: "Conversions", icon: "mdi-flag" },
+        {
+          category: "티셔츠",
+          title: "판매품목1",
+          text: "Greyhound divisely hello coldly fonwderfully",
+          icon: "mdi-clock",
+        },
+        {
+          category: "신발",
+          title: "판매품목2",
+          text: "Lorem sit velit adipisicing minim non esse officia minim culpa tempor.",
+          icon: "mdi-account",
+        },
+        {
+          category: "데님",
+          title: "판매품목3",
+          text: "Greyhound divisely hello coldly fonwderfully",
+          icon: "mdi-flag",
+        },
+        {
+          category: "면바지",
+          title: "판매품목4",
+          text: "Mollit fugiat minim laborum nisi veniam eu labore culpa aliqua dolore voluptate.",
+          icon: "mdi-flag",
+        },
+        {
+          category: "기타",
+          title: "판매품목5",
+          text: "Greyhound divisely hello coldly fonwderfully",
+          icon: "mdi-flag",
+        },
+        {
+          category: "슬랙스",
+          title: "판매품목6",
+          text: "Minim et labore ea veniam cupidatat consectetur proident voluptate eu cillum est cupidatat dolore.",
+          icon: "mdi-flag",
+        },
+        {
+          category: "아우터",
+          title: "판매품목7",
+          text: "Greyhound divisely hello coldly fonwderfully",
+          icon: "mdi-flag",
+        },
       ],
     };
   },
--- src/components/list_item.vue
+++ src/components/list_item.vue
@@ -2,14 +2,30 @@
   <v-card variant>
     <v-card-header>
       <div>
-        <div class="text-overline mb-1">OVERLINE</div>
-        <div class="text-h6 mb-1">Headline</div>
-        <div class="text-caption">Greyhound divisely hello coldly fonwderfully</div>
+        <div class="text-overline mb-1">{{category}}</div>
+        <div class="text-h6 mb-1">{{title}}</div>
+        <div class="text-caption">{{desc}}</div>
       </div>
     </v-card-header>
-
-    <!-- <v-card-actions>
-          <v-btn variant="outlined" rounded text>Button</v-btn>
-    </v-card-actions>-->
   </v-card>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  props: {
+    title: {
+      type: String,
+      default: "",
+    },
+    category: {
+      type: String,
+      default: "",
+    },
+    desc: {
+      type: String,
+      default: "Greyhound divisely hello coldly fonwderfully",
+    },
+  },
+});
+</script>
